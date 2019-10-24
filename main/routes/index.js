0e035e466b793e4d22046046c497689796cc2379
Aydee-qm <aydee.qm@gmail.com>
2019-10-23 22:09:33 -0500

    update configuration of routes

--- main/routes/index.js
+++ main/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (require, response) => {
-  res.send('GET route on things.');
+router.get('/', (request, response) => {
+  response.send('GET route on things.');
 });
-router.post('/', (require, response) => {
-  res.send('POST route on things.');
+
+router.post('/', (request, response) => {
+  response.send('POST route on things.');
 });
 
 module.exports = router;
