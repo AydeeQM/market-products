cbbe7d220a78dd0e23406dbc3b8e215bf891adc5
Jean <rjeangonzalo@gmail.com>
2019-10-23 20:47:31 -0500

    agregando la base de datos

---
+++ db.js
@@ -0,0 +1,19 @@
+const products = [{
+        idprod: 'a154b48r',
+        name: 'poleras',
+        price: 450.00,
+        description: 'invierno',
+        owner: 'ppppp'
+    },
+    {
+        idprod: 'b47b77b',
+        name: 'sueter',
+        price: 80.00,
+        description: 'invierno',
+        owner: 'aaaaa'
+    }
+]
+
+module.exports = {
+    products
+}
